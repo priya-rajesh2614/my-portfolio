264c3b20a7dcc1eac79d4f6f3871163c3f595fc1
priya-rajesh2614 <priya.rajesh26143@gmail.com>
2026-09-23 23:10:59 +0530

    Fix image source path in project HTML generation

--- assets/js/script.js
+++ assets/js/script.js
@@ -145,7 +145,7 @@ function showProjects(projects) {
     projects.forEach(project => {
         projectHTML += `
         <div class="box tilt">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+      <img draggable="false" src="/assets/image/${project.image}.png" alt="project" />
       <div class="content">
         <div class="tag">
         <h3>${project.name}</h3>
